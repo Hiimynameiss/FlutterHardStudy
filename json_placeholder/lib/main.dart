99c37fae6f84a2c86c5643d8ad7ecf8c28739133
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-01-30 21:54:09 +0900

    [2026-01-30] #5 FutureBuilder 예시 코드

--- json_placeholder/lib/main.dart
+++ json_placeholder/lib/main.dart
@@ -49,6 +49,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
    */
 
+  Future<String> getData() async {
+    await Future.delayed(const Duration(seconds: 4));
+    return 'You have pushed the button this many times:';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +65,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            FutureBuilder<String> (
+              future: getData(),
+              builder: (context, asyncSnapshot) {
+                if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return Text(
+                  asyncSnapshot.data!,
+                );
+              }),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
